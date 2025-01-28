8fafc045a9ba5058263abf371c12618519d9b885
reginaldbdanso <reginaldbdanso@gmail.com>
2025-01-28 19:54:57 +0000

    Added server.js and backend package.json files

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const jobRoutes = require('./routes/jobs')
+const blogRoutes = require('./routes/blog')
+const adminRoutes = require('./routes/admin')
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.use('/api/v1/jobs', jobRoutes );
+app.use('/api/v1/blog', blogRoutes );
+app.use('/api/v1/admin', adminRoutes );
+
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    }).catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    })
